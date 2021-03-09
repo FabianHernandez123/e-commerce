a2de0101d5e96396168f8b9bd5dcb9fc32eedd54
Fabian Hernandez <h.fabian25f@gmail.com>
2021-03-09 11:00:52 -0600

    Day 2 of Ecommerce

---
+++ src/main/java/com/tts/ecommerce/model/Product.java
@@ -0,0 +1,32 @@
+package com.tts.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private Long id;
+    private Integer quantity;
+    //be cautious about using float for price
+    private float price;
+
+    private String brand;
+    private String category;
+    private String description;
+    private String size;
+    private String name;
+    private String image;
+
+
+}
---
+++ src/main/java/com/tts/ecommerce/model/UserDetails.java
@@ -0,0 +1,11 @@
+package com.tts.ecommerce.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Transient;
+import java.util.Collection;
+
+public class UserDetails {
+
+
+}
